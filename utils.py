e454ad5245d52d560194dfcea32a6052ef24bf24
KAI10 <ikaziashik@gmail.com>
2020-04-21 19:18:52 -0400

    implemented model from paper

---
+++ utils.py
@@ -0,0 +1,10 @@
+import numpy as np
+import scipy.sparse as sp
+
+def normalize_adj(mx):
+    """Row-normalize sparse matrix"""
+    rowsum = np.array(mx.sum(1))
+    r_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    r_inv_sqrt[np.isinf(r_inv_sqrt)] = 0.
+    r_mat_inv_sqrt = sp.diags(r_inv_sqrt)
+    return mx.dot(r_mat_inv_sqrt).transpose().dot(r_mat_inv_sqrt)
